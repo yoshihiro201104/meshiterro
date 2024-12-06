7b5701f53aa19e8411013cb52361b2c5e11f37a2
yoshihiro201104 <187289075+yoshihiro201104@users.noreply.github.com>
2024-12-06 06:06:03 +0000

    [Update]10-6

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
+  get 'images/new'
+  get 'images/index'
+  get 'images/show'
+  get 'images/edit'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "homes#top"
 end
