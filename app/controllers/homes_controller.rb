217304ceba526a6a9d7331393c9d7e8a8238e130
yoshihiro201104 <187289075+yoshihiro201104@users.noreply.github.com>
2024-12-06 08:15:31 +0000

    [Add]

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -2,4 +2,11 @@ class HomesController < ApplicationController
   def top
     # ここにトップページの処理を記述します
   end
+
+  def about
+  end
+
+  def about_path
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
   get 'images/edit'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "homes#top"
+  get 'homes/about' => 'homes#about'
 end
