b8932ced8074724014685f1acb43a6d76d255c87
yoshihiro201104 <187289075+yoshihiro201104@users.noreply.github.com>
2024-12-09 05:06:01 +0000

    [Finish]3-8

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'images/new'
-  get 'images/index'
-  get 'images/show'
-  get 'images/edit'
+
+  
+  resources :post_images, only: [:new, :index, :show]
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "homes#top"
   get 'homes/about' => 'homes#about'
