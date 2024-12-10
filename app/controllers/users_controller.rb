9d62e9580430377de35ac1a1ffb01774aee98eb0
yoshihiro201104 <187289075+yoshihiro201104@users.noreply.github.com>
2024-12-10 09:33:14 +0000

    [Finish]3-16

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,16 +7,16 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
   end
-end
-def update
-  @user = user.find(params[:id])
-  @user.update(list_params)
-  redirect_to user_path(user.id)
-end
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
+  end
 
 
-private
+  private
 
-def user_params
-  params.require(:user).permit(:name, :profile_image)
+  def user_params
+    params.require(:user).permit(:name, :profile_image)
+  end
 end
